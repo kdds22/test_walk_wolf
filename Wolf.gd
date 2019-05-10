8b78070cefc4eac9c95173824bc1dc08c93885c2
Klaus Dellano <klausdell@hotmail.com>
2019-05-10 10:29:57 -0300

    Added Black Wolf Sprite

--- Wolf.gd
+++ Wolf.gd
@@ -4,8 +4,23 @@ extends KinematicBody2D
 export (int, 100, 1000, 50) var GRAVITY = 100
 export (int, 10, 1000, 10) var speed = 300
 
+export (int, 1, 2, 1) var Wolf_type
+#export (String, FILE, "*png") var White_Wolf
+#export (String, FILE, "*png") var Black_Wolf
+
+var white_wolf_sprite = "res://assets/wolf-Sheet.png"
+var black_wolf_sprite = "res://assets/black-wolf-Sheet.png"
+
+
 var velocity = Vector2()
 
+func _ready():
+	if Wolf_type == 1:
+		$Sprite.texture = load(white_wolf_sprite)
+	elif Wolf_type == 2:
+		$Sprite.texture = load(black_wolf_sprite)
+	else:
+		$Sprite.texture = white_wolf_sprite
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
